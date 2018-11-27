13e40f8462e0cda1c654fabd86ba4625b626dbe7
Dmytro Lysai <d@emphased.net>
2018-11-28 01:05:11 +0200

    Add Direction::rotate_*()

--- src/graphics/geometry/mod.rs
+++ src/graphics/geometry/mod.rs
@@ -2,6 +2,8 @@ pub mod hex;
 pub mod map;
 pub mod sqr;
 
+use util::EnumExt;
+
 #[derive(Clone, Copy, Debug, Enum, PartialEq, PartialOrd, Primitive)]
 pub enum Direction {
     NE  = 0,
@@ -19,6 +21,18 @@ impl Direction {
         static VALUES: [Direction; Direction::LEN] =
             [Direction::NE, Direction::E, Direction::SE, Direction::SW, Direction::W, Direction::NW];
         &VALUES[..]
+    }
+
+    pub fn rotate_cw(self) -> Self {
+        Self::from_ordinal((self.ordinal() + 1) % Self::len())
+    }
+
+    pub fn rotate_ccw(self) -> Self {
+        let mut o = self.ordinal() as isize - 1;
+        if o < 0 {
+            o += Self::len() as isize;
+        }
+        Self::from_ordinal(o as usize)
     }
 }
 
