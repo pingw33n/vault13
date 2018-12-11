c33571a2d7b4a0fdd34c1975765c429fca2fbfb4
Dmytro Lysai <d@emphased.net>
2018-12-11 23:45:17 +0200

    Add outline support in Sprite

--- src/graphics/frm.rs
+++ src/graphics/frm.rs
@@ -1,10 +1,11 @@
 use enum_map::EnumMap;
+use std::cell::Ref;
 
 use asset::frm::{Fid, FrmDb};
 use graphics::color::*;
 use graphics::geometry::Direction;
 use graphics::{Point, Rect};
-use graphics::render::{Renderer, TextureHandle};
+use graphics::render::{Outline, Renderer, TextureHandle};
 
 #[derive(Clone, Debug)]
 pub struct FrameSet {
@@ -33,7 +34,7 @@ pub struct Frame {
     pub texture: TextureHandle,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Enum, Eq, PartialEq)]
 pub enum Translucency {
     Energy,
     Glass,
@@ -42,12 +43,27 @@ pub enum Translucency {
     Wall,
 }
 
+#[derive(Clone, Copy, Debug, Enum, Eq, PartialEq)]
+pub enum OutlineStyle {
+    GlowingRed,
+    Red,
+    Gray,
+    GlowingGreen,
+    Yellow,
+    Brown,
+    Purple,
+}
+
 #[derive(Clone, Copy, Debug)]
 pub enum Effect {
     Translucency(Translucency),
     Masked {
         mask_pos: Point,
         mask_fid: Fid,
+    },
+    Outline {
+        style: OutlineStyle,
+        translucent: bool,
     },
 }
 
@@ -68,20 +84,10 @@ impl Sprite {
         let frml = &frms.frame_lists[self.direction];
         let frm = &frml.frames[self.frame_idx];
 
-        fn compute_bounds(p: Point, center: Point, frm: &Frame) -> Rect {
-            let p = p + center;
-            Rect {
-                left: p.x - frm.width / 2,
-                top: p.y - frm.height + 1,
-                right: p.x + frm.width / 2,
-                bottom: p.y + 1,
-            }
-        }
-
         let bounds = if self.centered {
-            compute_bounds(self.pos, frml.center, frm)
+            Self::bounds_centered(self.pos, frml.center, frm)
         } else {
-            Rect::with_size(self.pos.x, self.pos.y, frm.width, frm.height - 1)
+            Rect::with_size(self.pos.x, self.pos.y, frm.width, frm.height)
         };
 
         match self.effect {
@@ -100,14 +106,54 @@ impl Sprite {
                 let mask_frms = frm_db.get(mask_fid);
                 let mask_frml = &mask_frms.frame_lists[Direction::NE];
                 let mask_frm = &mask_frml.frames[0];
-                let mask_bounds = compute_bounds(mask_pos, mask_frml.center, &mask_frm);
+                let mask_bounds = Self::bounds_centered(mask_pos, mask_frml.center, &mask_frm);
                 renderer.draw_masked(&frm.texture, bounds.left, bounds.top,
                     &mask_frm.texture, mask_bounds.left, mask_bounds.top,
                     self.light);
+            }
+            Some(Effect::Outline { style, translucent }) => {
+                use self::OutlineStyle::*;
+                let trans_color = if translucent { Some(()) } else { None };
+                let outline = match style {
+                    GlowingRed => GLOWING_RED_OUTLINE,
+                    Red => Outline::Fixed {
+                        color: Rgb15::from_packed(0x7c00),
+                        trans_color: trans_color.map(|_| TRANS_RED),
+                    },
+                    Gray => Outline::Fixed {
+                        color: Rgb15::from_packed(0x3def),
+                        trans_color: trans_color.map(|_| TRANS_WALL),
+                    },
+                    GlowingGreen => GLOWING_GREEN_OUTLINE,
+                    Yellow => Outline::Fixed {
+                        color: Rgb15::from_packed(0x77a8),
+                        trans_color: trans_color.map(|_| TRANS_RED),
+                    },
+                    Brown => Outline::Fixed {
+                        // Originally this was color index 61, do we need support this?
+                        color: Rgb15::from_packed(0x5226),
+                        trans_color: None,
+                    },
+                    Purple => Outline::Fixed {
+                        color: Rgb15::from_packed(0x7c1f),
+                        trans_color: None,
+                    },
+                };
+                renderer.draw_outline(&frm.texture, bounds.left - 1, bounds.top - 1, outline);
             }
             None => renderer.draw(&frm.texture, bounds.left, bounds.top, self.light),
         }
 
         bounds
+    }
+
+    fn bounds_centered(p: Point, center: Point, frm: &Frame) -> Rect {
+        let p = p + center;
+        Rect {
+            left: p.x - frm.width / 2,
+            top: p.y - frm.height + 1,
+            right: p.x + frm.width / 2,
+            bottom: p.y + 1,
+        }
     }
 }
