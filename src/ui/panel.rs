159501bb28fc77ef55ee248cd1e9b769e9fd5aa3
Dmytro Lysai <d@emphased.net>
2020-06-10 13:35:47 +0300

    [ui] Add text to Panel

--- src/ui/panel.rs
+++ src/ui/panel.rs
@@ -1,17 +1,54 @@
+use bstring::BString;
+
+use crate::graphics::color::Rgb15;
+use crate::graphics::font::{DrawOptions, FontKey};
 use super::*;
 
-/// Simple widget without any added functions. Can be used to utilize what base widget features
-/// (background, cursor etc).
-pub struct Panel {}
+#[derive(Clone, Debug)]
+pub struct Text {
+    pub text: BString,
+    pub font: FontKey,
+    pub color: Rgb15,
+    pub options: DrawOptions,
+}
+
+/// Simple widget that can be used to utilize base widget features (background, cursor etc) and
+/// to draw text.
+pub struct Panel {
+    text: Option<Text>,
+}
 
 impl Panel {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            text: None,
+        }
+    }
+
+    pub fn text(&self) -> Option<&Text> {
+        self.text.as_ref()
+    }
+
+    pub fn text_mut(&mut self) -> Option<&mut Text> {
+        self.text.as_mut()
+    }
+
+    pub fn set_text(&mut self, text: Option<Text>) {
+        self.text = text;
     }
 }
 
 impl Widget for Panel {
     fn handle_event(&mut self, _ctx: HandleEvent) {}
 
-    fn render(&mut self, _ctx: Render) {}
+    fn render(&mut self, ctx: Render) {
+        if let Some(text) = self.text() {
+            ctx.canvas.draw_text(
+                &text.text,
+                ctx.base.unwrap().rect.top_left(),
+                text.font,
+                text.color,
+                &text.options);
+        }
+    }
 }
