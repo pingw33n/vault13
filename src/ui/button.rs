a3a1bd6c2e0b24a50ace56f47af2ab818ccac513
Dmytro Lysai <d@emphased.net>
2020-06-10 13:50:06 +0300

    [ui] Add text to Button

--- src/ui/button.rs
+++ src/ui/button.rs
@@ -1,17 +1,46 @@
+use bstring::BString;
 use enum_map::{enum_map, EnumMap};
 
-use super::*;
+use crate::graphics::font::{DrawOptions, FontKey, HorzAlign, VertAlign};
+use crate::graphics::color::Rgb15;
 use crate::graphics::sprite::Sprite;
 use crate::ui::command::UiCommandData;
+use super::*;
 
 #[derive(Clone, Copy, Debug, Enum, Eq, PartialEq, Ord, PartialOrd)]
-enum State {
+pub enum State {
     Up,
     Down,
 }
 
+pub struct Config {
+    pub background: Option<Sprite>,
+    pub text: Option<Text>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Text {
+    pub pos: Point,
+    pub text: BString,
+    pub font: FontKey,
+    pub color: Rgb15,
+    pub options: DrawOptions,
+}
+
+impl Text {
+    pub fn new(text: BString, font: FontKey) -> Self {
+        Self {
+            text,
+            font,
+            pos: Default::default(),
+            color: Default::default(),
+            options: Default::default(),
+        }
+    }
+}
+
 pub struct Button {
-    sprites: EnumMap<State, Sprite>,
+    configs: EnumMap<State, Config>,
     command: Option<UiCommandData>,
     state: State,
 }
@@ -19,13 +48,32 @@ pub struct Button {
 impl Button {
     pub fn new(up: FrameId, down: FrameId, command: Option<UiCommandData>) -> Self {
         Self {
-            sprites: enum_map! {
-                State::Up => Sprite::new(up),
-                State::Down => Sprite::new(down),
+            configs: enum_map! {
+                State::Up => Config {
+                    background: Some(Sprite::new(up)),
+                    text: None,
+                },
+                State::Down => Config {
+                    background: Some(Sprite::new(down)),
+                    text: None,
+                },
             },
             command,
             state: State::Up,
         }
+    }
+
+    pub fn config(&self, state: State) -> &Config {
+        &self.configs[state]
+    }
+
+    pub fn config_mut(&mut self, state: State) -> &mut Config {
+        &mut self.configs[state]
+    }
+
+    pub fn set_text(&mut self, text: Option<Text>) {
+        self.configs[State::Down].text = text.clone();
+        self.configs[State::Up].text = text;
     }
 }
 
@@ -59,8 +107,26 @@ impl Widget for Button {
     }
 
     fn render(&mut self, ctx: Render) {
-        let sprite = &mut self.sprites[self.state];
-        sprite.pos = ctx.base.unwrap().rect.top_left();
-        sprite.render(ctx.canvas, ctx.frm_db);
+        let config = &self.configs[self.state];
+        let base_rect = ctx.base.unwrap().rect;
+        if let Some(mut background) = config.background {
+            background.pos += base_rect.top_left();
+            background.render(ctx.canvas, ctx.frm_db);
+        }
+        if let Some(text) = config.text.as_ref() {
+            let mut pos = base_rect.top_left() + text.pos;
+            if text.options.horz_align == HorzAlign::Center {
+                pos.x += base_rect.width() / 2;
+            }
+            if text.options.vert_align == VertAlign::Middle {
+                pos.y += base_rect.height() / 2;
+            }
+            ctx.canvas.draw_text(
+                &text.text,
+                pos,
+                text.font,
+                text.color,
+                &text.options);
+        }
     }
 }
