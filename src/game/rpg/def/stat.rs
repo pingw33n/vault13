bc389c47554413f72273f24dd0bb1eda770497c7
Dmytro Lysai <d@emphased.net>
2020-07-02 10:42:50 +0300

    [game,rpg] Use enum_map! in StaDef::defaults()

--- src/game/rpg/def/stat.rs
+++ src/game/rpg/def/stat.rs
@@ -25,50 +25,48 @@ impl StatDef {
         }
     }
 
-    pub fn defaults() -> EnumMap<Stat, StatDef> {
-        EnumMap::from(|stat| STAT_DEFS[stat as usize].clone())
+    pub fn defaults() -> EnumMap<Stat, Self> {
+        use Stat::*;
+        enum_map! {
+            Strength => Self::new(0, 1, 10, 5),
+            Perception => Self::new(1, 1, 10, 5),
+            Endurance => Self::new(2, 1, 10, 5),
+            Charisma => Self::new(3, 1, 10, 5),
+            Intelligence => Self::new(4, 1, 10, 5),
+            Agility => Self::new(5, 1, 10, 5),
+            Luck => Self::new(6, 1, 10, 5),
+            HitPoints => Self::new(10, 0, 999, 0),
+            ActionPoints => Self::new(75, 1, 99, 0),
+            ArmorClass => Self::new(18, 0, 999, 0),
+            UnarmedDmg => Self::new(31, 0, i32::max_value(), 0),
+            MeleeDmg => Self::new(32, 0, 500, 0),
+            CarryWeight => Self::new(20, 0, 999, 0),
+            Sequence => Self::new(24, 0, 60, 0),
+            HealRate => Self::new(25, 0, 30, 0),
+            CritChance => Self::new(26, 0, 100, 0),
+            BetterCrit => Self::new(94, -60, 100, 0),
+            DmgThresh => Self::new(0, 0, 100, 0),
+            DmgThreshLaser => Self::new(0, 0, 100, 0),
+            DmgThreshFire => Self::new(0, 0, 100, 0),
+            DmgThreshPlasma => Self::new(0, 0, 100, 0),
+            DmgThreshElectrical => Self::new(0, 0, 100, 0),
+            DmgThreshEmp => Self::new(0, 0, 100, 0),
+            DmgThreshExplosion => Self::new(0, 0, 100, 0),
+            DmgResist => Self::new(22, 0, 90, 0),
+            DmgResistLaser => Self::new(0, 0, 90, 0),
+            DmgResistFire => Self::new(0, 0, 90, 0),
+            DmgResistPlasma => Self::new(0, 0, 90, 0),
+            DmgResistElectrical => Self::new(0, 0, 90, 0),
+            DmgResistEmp => Self::new(0, 0, 100, 0),
+            DmgResistExplosion => Self::new(0, 0, 90, 0),
+            RadResist => Self::new(83, 0, 95, 0),
+            PoisonResist => Self::new(23, 0, 95, 0),
+            Age => Self::new(0, 16, 101, 25),
+            Gender => Self::new(0, 0, 1, 0),
+            CurrentHitPoints => Self::new(10, 0, 2000, 0),
+            CurrentPoison => Self::new(11, 0, 2000, 0),
+            CurrentRad => Self::new(12, 0, 2000, 0),
+        }
     }
 }
-
-static STAT_DEFS: &[StatDef] = &[
-    StatDef::new(0, 1, 10, 5), // Strength
-    StatDef::new(1, 1, 10, 5), // Perception
-    StatDef::new(2, 1, 10, 5), // Endurance
-    StatDef::new(3, 1, 10, 5), // Charisma
-    StatDef::new(4, 1, 10, 5), // Intelligence
-    StatDef::new(5, 1, 10, 5), // Agility
-    StatDef::new(6, 1, 10, 5), // Luck
-    StatDef::new(10, 0, 999, 0), // HitPoints
-    StatDef::new(75, 1, 99, 0), // ActionPoints
-    StatDef::new(18, 0, 999, 0), // ArmorClass
-    StatDef::new(31, 0, i32::max_value(), 0), // UnarmedDmg
-    StatDef::new(32, 0, 500, 0), // MeleeDmg
-    StatDef::new(20, 0, 999, 0), // CarryWeight
-    StatDef::new(24, 0, 60, 0), // Sequence
-    StatDef::new(25, 0, 30, 0), // HealRate
-    StatDef::new(26, 0, 100, 0), // CritChance
-    StatDef::new(94, -60, 100, 0), // BetterCrit
-    StatDef::new(0, 0, 100, 0), // DmgThresh
-    StatDef::new(0, 0, 100, 0), // DmgThreshLaser
-    StatDef::new(0, 0, 100, 0), // DmgThreshFire
-    StatDef::new(0, 0, 100, 0), // DmgThreshPlasma
-    StatDef::new(0, 0, 100, 0), // DmgThreshElectrical
-    StatDef::new(0, 0, 100, 0), // DmgThreshEmp
-    StatDef::new(0, 0, 100, 0), // DmgThreshExplosion
-    StatDef::new(22, 0, 90, 0), // DmgResist
-    StatDef::new(0, 0, 90, 0), // DmgResistLaser
-    StatDef::new(0, 0, 90, 0), // DmgResistFire
-    StatDef::new(0, 0, 90, 0), // DmgResistPlasma
-    StatDef::new(0, 0, 90, 0), // DmgResistElectrical
-    StatDef::new(0, 0, 100, 0), // DmgResistEmp
-    StatDef::new(0, 0, 90, 0), // DmgResistExplosion
-    StatDef::new(83, 0, 95, 0), // RadResist
-    StatDef::new(23, 0, 95, 0), // PoisonResist
-    StatDef::new(0, 16, 101, 25), // Age
-    StatDef::new(0, 0, 1, 0), // Gender
-    StatDef::new(10, 0, 2000, 0), // CurrentHitPoints
-    StatDef::new(11, 0, 2000, 0), // CurrentPoison
-    StatDef::new(12, 0, 2000, 0), // CurrentRad
-];
-
 
